3b5f1c08836edc02330ea84252a9dc0b0356f442
Skidam <67871298+Skidamek@users.noreply.github.com>
2023-10-20 20:39:09 +0200

    Update ForgeLanguageProviderCallback.java
    Makes preloading tricks work on forge < 41.0.39

--- fml-45/src/main/java/settingdust/preloadingtricks/forge/ForgeLanguageProviderCallback.java
+++ fml-45/src/main/java/settingdust/preloadingtricks/forge/ForgeLanguageProviderCallback.java
@@ -2,7 +2,6 @@ package settingdust.preloadingtricks.forge;
 
 import net.minecraftforge.fml.loading.EarlyLoadingException;
 import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.fml.loading.moddiscovery.BackgroundScanHandler;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 import net.minecraftforge.fml.loading.moddiscovery.ModValidator;
 import net.minecraftforge.forgespi.locating.IModFile;
@@ -12,82 +11,99 @@ import settingdust.preloadingtricks.SetupModCallback;
 import settingdust.preloadingtricks.SetupModService;
 import settingdust.preloadingtricks.util.ServiceLoaderUtil;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.ServiceLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("unchecked")
 public class ForgeLanguageProviderCallback implements LanguageProviderCallback {
-    private final Field fieldModValidator;
-    private final ModValidator validator;
+    private final List<ModFile> candidateMods;
 
-    private final Field fieldCandidateMods;
-    private List<ModFile> candidateMods;
-
-    public ForgeLanguageProviderCallback() throws NoSuchFieldException, IllegalAccessException {
-        fieldModValidator = FMLLoader.class.getDeclaredField("modValidator");
+    public ForgeLanguageProviderCallback() throws NoSuchFieldException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Field fieldModValidator = FMLLoader.class.getDeclaredField("modValidator");
         fieldModValidator.setAccessible(true);
 
-        fieldCandidateMods = ModValidator.class.getDeclaredField("candidateMods");
+        Field fieldCandidateMods = ModValidator.class.getDeclaredField("candidateMods");
         fieldCandidateMods.setAccessible(true);
 
-        validator = (ModValidator) fieldModValidator.get(null);
+        ModValidator validator = (ModValidator) fieldModValidator.get(null);
         // Proxy won't work with non interface
-        fieldModValidator.set(null, new DummyModValidator());
-
-        new ForgeModSetupService();
-    }
-
-    private void setupModsInvoking() throws IllegalAccessException {
+        validator = ModValidator(validator);
         fieldModValidator.set(null, validator);
         candidateMods = (List<ModFile>) fieldCandidateMods.get(validator);
+
         ServiceLoaderUtil.loadServices(
                 SetupModCallback.class,
                 ServiceLoader.load(SetupModCallback.class, SetupModCallback.class.getClassLoader()),
                 LogManager.getLogger("PreloadingTricks/ModSetup"));
+
+        new ForgeModSetupService();
     }
 
-    private class DummyModValidator extends ModValidator {
-        private static final Field fieldModFiles;
+    public record ModValidatorRecord(Integer parameters, Constructor<?> constructor) {}
 
-        private static final Field fieldBrokenFiles;
+    public ModValidatorRecord getValidatorConstructor() {
+        Class<?> modValidatorClass = ModValidator.class;
+        Constructor<?>[] constructors = modValidatorClass.getConstructors();
+        for (Constructor<?> constructor : constructors) { // This method has just one constructor
+            return new ModValidatorRecord(constructor.getParameterCount(), constructor);
+        }
+        return new ModValidatorRecord(-1, null);
+    }
 
-        private static final Field fieldDiscoveryErrorData;
+    public ModValidator createInstance(Constructor<?> constructor, Object[] parameters) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        return (ModValidator) constructor.newInstance(parameters);
+    }
 
-        static {
-            try {
-                fieldModFiles = ModValidator.class.getDeclaredField("modFiles");
-                fieldBrokenFiles = ModValidator.class.getDeclaredField("brokenFiles");
-                fieldDiscoveryErrorData = ModValidator.class.getDeclaredField("discoveryErrorData");
+    public ModValidator ModValidator(ModValidator validator) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Field fieldModFiles = ModValidator.class.getDeclaredField("modFiles");
+        Field fieldBrokenFiles = ModValidator.class.getDeclaredField("brokenFiles");
+        Field fieldDiscoveryErrorData = ModValidator.class.getDeclaredField("discoveryErrorData");
 
-                fieldModFiles.setAccessible(true);
-                fieldBrokenFiles.setAccessible(true);
-                fieldDiscoveryErrorData.setAccessible(true);
-            } catch (NoSuchFieldException e) {
-                throw new RuntimeException(e);
-            }
+        fieldModFiles.setAccessible(true);
+        fieldBrokenFiles.setAccessible(true);
+        fieldDiscoveryErrorData.setAccessible(true);
+
+        var modFiles = (Map<IModFile.Type, List<ModFile>>) fieldModFiles.get(validator);
+        var brokenFiles = ((List<IModFile>) fieldBrokenFiles.get(validator)).stream().map(IModFile::getModFileInfo).collect(Collectors.toList());
+        var exceptionData = (List<EarlyLoadingException.ExceptionData>) fieldDiscoveryErrorData.get(validator);
+
+
+        var validatorConstructor = getValidatorConstructor();
+        if (validatorConstructor.parameters == 3) {
+
+            // forge >= 41.0.39
+
+            // See constructor of ModValidator class
+            Object[] parameters = {
+                    modFiles,
+                    brokenFiles,
+                    exceptionData
+            };
+
+            return createInstance(validatorConstructor.constructor, parameters);
+        } else if (validatorConstructor.parameters == 2) {
+
+            // forge < 41.0.39
+
+            // See https://github.com/MinecraftForge/MinecraftForge/blob/9523232a377e0704b2c016f0665bb15571ab5ac8/fmlloader/src/main/java/net/minecraftforge/fml/loading/moddiscovery/ModValidator.java#L31C47-L31C56
+            Object[] parameters = {
+                    modFiles,
+                    exceptionData
+            };
+
+            ModValidator modValidator = createInstance(validatorConstructor.constructor, parameters);
+
+            // We need to manually set this to initialize the list, if we don't do that, forge gonna crash.
+            fieldBrokenFiles.set(modValidator, brokenFiles);
+
+            return modValidator;
         }
 
-        public DummyModValidator() throws IllegalAccessException {
-            super(
-                    (Map<IModFile.Type, List<ModFile>>) fieldModFiles.get(validator),
-                    ((List<IModFile>) fieldBrokenFiles.get(validator))
-                            .stream().map(IModFile::getModFileInfo).collect(Collectors.toList()),
-                    (List<EarlyLoadingException.ExceptionData>) fieldDiscoveryErrorData.get(validator));
-        }
-
-        @Override
-        public BackgroundScanHandler stage2Validation() {
-            try {
-                setupModsInvoking();
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-            return validator.stage2Validation();
-        }
+        throw new RuntimeException("Couldn't find any valid constructor for ModValidator class");
     }
 
     public class ForgeModSetupService implements SetupModService<ModFile> {

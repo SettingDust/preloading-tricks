b4f5e410628e428d925a77059bdd48235e8d21c0
SettingDust <settingdust@gmail.com>
2024-09-15 20:16:53 +0800

    ci: enable neoforge api and language-loader scripts
    Relate: #6

--- neoforge/api/build.gradle.kts
+++ neoforge/api/build.gradle.kts
@@ -1,17 +1,17 @@
-//plugins {
-//    alias(catalog.plugins.neoforge.gradle)
-//}
-//
-//afterEvaluate {
-//    runs.clear()
-//}
-//
-//jarJar.enable()
-//
-//dependencies {
-//    implementation(catalog.neoforge)
-//
-//    implementation(project(":services")) {
-//        isTransitive = false
-//    }
-//}
+plugins {
+    alias(catalog.plugins.neoforge.gradle)
+}
+
+afterEvaluate {
+    runs.clear()
+}
+
+jarJar.enable()
+
+dependencies {
+    implementation(catalog.neoforge)
+
+    implementation(project(":services")) {
+        isTransitive = false
+    }
+}
--- neoforge/language-loader/build.gradle.kts
+++ neoforge/language-loader/build.gradle.kts
@@ -1,23 +1,23 @@
-//plugins {
-//    alias(catalog.plugins.neoforge.gradle)
-//}
-//
-//val mod_id: String by rootProject
-//
-//minecraft {
-//    modIdentifier("${mod_id}_language_loader")
-//
-//    afterEvaluate {
-//        runs.clear()
-//    }
-//}
-//
-//jarJar.enable()
-//
-//dependencies {
-//    implementation(catalog.neoforge)
-//
-//    implementation(project(":services")) {
-//        isTransitive = false
-//    }
-//}
+plugins {
+    alias(catalog.plugins.neoforge.gradle)
+}
+
+val mod_id: String by rootProject
+
+minecraft {
+    modIdentifier("${mod_id}_language_loader")
+
+    afterEvaluate {
+        runs.clear()
+    }
+}
+
+jarJar.enable()
+
+dependencies {
+    implementation(catalog.neoforge)
+
+    implementation(project(":services")) {
+        isTransitive = false
+    }
+}

b8b17db031b9731f39cb7bd63be22b4f061193a7
SettingDust <settingdust@gmail.com>
2025-10-26 18:54:43 +0800

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,11 @@ plugins {
     java
     idea
 
-    id("com.palantir.git-version") version "3.1.0"
+    id("com.palantir.git-version") version "4.1.0"
 
-    id("com.gradleup.shadow") version "9.0.2"
+    id("com.gradleup.shadow") version "9.2.2"
 
-    id("earth.terrarium.cloche") version "0.16.0-dust"
+    id("earth.terrarium.cloche") version "0.16.1"
 }
 
 val archive_name: String by rootProject.properties

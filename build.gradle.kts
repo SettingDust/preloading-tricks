5e85da5386b3979fb0eca7484e7a1b0b79b64f05
SettingDust <settingdust@gmail.com>
2025-11-27 11:51:24 +0800

    build: bump cloche to fix crash with asm fabric loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 
     id("com.gradleup.shadow") version "9.2.2"
 
-    id("earth.terrarium.cloche") version "0.16.20-dust"
+    id("earth.terrarium.cloche") version "0.16.21-dust"
 }
 
 val archive_name: String by rootProject.properties
